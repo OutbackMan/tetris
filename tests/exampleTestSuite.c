2620ef1ad5fb9b53d1a03645e2443bf4287deb62
Ryan <re.mcclue@gmail.com>
2018-05-09 13:27:54 +1000

    near

--- tests/exampleTestSuite.c
+++ tests/exampleTestSuite.c
@@ -1,24 +1,25 @@
 #include "g_test-helpers.h"
 
-GAME_TEST_STATUS exampleTestSuite(int argc, char* argv[argc + 1])
+int example_test_a(void);
+int example_test_b(void);
+
+int tests_exampleTestSuite(int argc, char* argv[argc + 1])
 {
 	if (game_tests_strcmp(argv[1], "example_test_a")) {
 		return example_test_a();
 	} else if (game_tests_strcmp(argv[1], "example_test_b")) {
 		return example_test_b();
 	} else {
-		return SUCCESS;
+		return 0;
 	}
+}
 
+int example_test_a(void)
+{
 	return 0;
 }
 
-GAME_TEST_STATUS example_test_a(void)
+int example_test_b(void)
 {
-	return 10 == 10;
-}
-
-GAME_TEST_STATUS example_test_b(void)
-{
-	return 20 == 20;
+	return 0;
 }
