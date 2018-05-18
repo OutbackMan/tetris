6bf066d4159937e7905bd1ecf80c0a745ae5a1d7
Ryan <re.mcclue@gmail.com>
2018-05-18 15:59:32 +1000

    :australia:

--- include/core/game.h
+++ include/core/game.h
@@ -6,17 +6,26 @@
 GAME_COLD
 GAME_STATUS g_game_execute(void);
 
+// renderable
+typedef struct {
+	SDL_Texture* texture; // sprite
+	size_t texture_width;
+	size_t texture_height;
+} Renderable;
 
 // physics
 typedef struct {
-	float px;
-	float py;
+	float px; // move into renderable
+	float py; // move into renderable
 	float vx;
 	float vy;
 	float ax;
 	float ay;
 	float collision_radius;
+	float friction;
 	bool is_stable;
+	int bounce_before_death;
+	bool is_dead;
 } PhysicsObject;
 
 void physics_object_create(obj, x, y)
@@ -25,6 +34,39 @@ void physics_object_create(obj, x, y)
 	obj->py = y;
 	obj->collision_radius = 4.0f;
 	obj->is_stable = false;
+	obj->friction = 0.8f;
+	obj->is_dead = false
+	obj->bounce_before_death = -1;
+}
+
+// use this as you would an object create routine
+void boom(x, y, explosion_radius)
+{
+	// shockwave other entities
+	for (all_drawable_objects) {
+		float dx = obj->px - x;
+		float dy = obj->py - y;
+		float dist = sqrt(dx*dx + dy*dy);
+
+		if (dist < explosion_radius) {
+			obj->vx = (dx / dist) * radius;	
+			obj->vy = (dy / dist) * radius;	
+			obj->is_stable = false;
+		}
+	}	
+
+	for (int i = 0; i < (int)radius; ++i) {
+		drawable_objects.push(create_object);		
+	}
+}
+
+void create_example_object(obj, x, y)
+{
+	obj->vx = 10.0f * cosf((float)rand() / (float)RAND_MAX * 2.0f * PI);
+	obj->vy = 10.0f * sinf((float)rand() / (float)RAND_MAX * 2.0f * PI);
+	obj->radius = 1.0f;
+	obj->friction = 1.2f;
+	obj->bounce_before_death = 5;
 }
 
 void physics_object_draw(obj)
--- src/core/game.c
+++ src/core/game.c
@@ -131,7 +131,61 @@ GAME_INTERNAL GAME_STATUS game_loop(void)
 GAME_HOT
 GAME_INTERNAL void game_update(float delta_time)
 {
+	for (all_physics_objects) {
+		obj->ay += 2.0f; // gravity		
+
+		obj->vx += obj->ax * delta_time;
+		obj->vy += obj->ay * delta_time;
+
+		float potential_x = obj->px + obj->vx * delta_time;
+		float potential_y = obj->py + obj->vy * delta_time;
+
+		obj->ax = 0.0f;
+		obj->ay = 0.0f;
+		obj->is_stable = false;
+
+		float collision_angle = atan2f(obj->vx, obj->vy);
+		float response_x = 0.0f;
+		float response_y = 0.0f;
+		bool collision_encountered = false;
+
+		// circle and rectangle collisions take similar times to perform, so pick the one that makes most sense for context and that allows to minimise function calls
+		// iterate through semicircle
+		// have arbitrarily chosen small increment to ensure a sufficient amount of pixels are tested 
+		for (float r = collision_angle - PI / 2; r < collision_angle + PI / 2; r += PI / 8.0f) {
+			float test_pos_x = obj->collision_radius * cosf(r) + potential_x;
+			float test_pos_y = obj->collision_radius * sinf(r) + potential_y;
+
+			// clamp test values (always do when reading from memory)
+			if (map[(int)test_pos_y * map_width + (int)test_pos_x] != SKY) {
+				response_x += potential_x - test_pos_x;	
+				response_y += potential_y - test_pos_y;	
+				collision_encountered = true;
+			}
+
+		}
 		
+		if (collision_encountered) {
+			// response vector as normal to tangent of terrain
+			// used for reflection calculation for bounce
+			obj->stable = true;	
+			float velocity_magnitude = sqrtf(obj->px*obj->px + obj->py*obj->py);
+			float response_magnitude = sqrtf(response_x*response_x + response_y*response_y);
+			// calculate reflection vector
+			// account for friction
+		} else {
+			obj->px = potential_x;
+			obj->py = potential_y;
+		}
+
+		if (obj->is_dead) {
+			int on_dead_response = dead_response(obj);
+			if (on_dead_response == EXPLODE) {
+				boom(x, y);		
+			}
+		}
+
+	}
 }
 
 GAME_INTERNAL void game_render(SDL_Renderer* renderer)
@@ -233,7 +287,12 @@ GAME_INTERNAL GAME_STATUS game_handle_mouse_button_events(SDL_MouseButtonEvent* 
 {
 	if (event->button == SDL_BUTTON_MIDDLE) {
 		add_drawable_physics_object(mouse_x + camera_x, mouse_y + camera_y);		
-	}	
+	}
+
+	if (event->button == SDL_BUTTON_LEFT) {
+		add_20_debris_objects(mouse_x + camera_x, mouse_y + camera_y);
+		// append to list of game objects
+	}
 }
 
 GAME_INTERNAL GAME_STATUS game_handle_mouse_motion_events(SDL_MouseMotionEvent* mouse_motion_event)
