c5f19319daa89cb0d30380adc976e3cae29d40d4
Ryan Edward <32256825+OutbackMan@users.noreply.github.com>
2019-07-31 09:37:50 +1000

    Update anki-addon.py

--- anki-addon.py
+++ anki-addon.py
@@ -4,3 +4,17 @@ from aqt.utils import showInfo
 
 from aqt.qt import *
 
+def addon_functionality():
+    deck_id = mw.col.decks.id("Encoding")
+    deck = mw.col.decks.get(deck_id)
+    # look at REPL
+    
+    cardCount = mw.col.cardCount()
+    # show a message box
+    showInfo("Card count: %d" % cardCount)
+
+
+run_addon = QAction("addon", mw)
+run_addon.triggered.connect(addon_functionality)
+
+mw.form.menuTools.addAction(run_addon)
